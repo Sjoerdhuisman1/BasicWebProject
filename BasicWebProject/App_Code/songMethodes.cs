d83d5c29852636a4963ede3403777c274caff311
Sjoerd Huisman <97072541@st.deltion.nl>
2020-02-28 22:09:21 +0100

    hallo

--- BasicWebProject/App_Code/songMethodes.cs
+++ BasicWebProject/App_Code/songMethodes.cs
@@ -9,35 +9,36 @@ namespace BasicWebProject.App_Code
 {
     public class songMethodes
     {
-    }
 
 
-    public DataSet GetAllSongs(string file)
-    {
-        DataSet ds = new DataSet("playlist");
 
-        DataTable dtSongs = new DataTable("songs");
-        
-        DataColumn dcId = new DataColumn("id");
-        DataColumn dcArtist = new DataColumn("artist");
-        DataColumn dcTitle = new DataColumn("title");
-        DataColumn dcYear = new DataColumn("year");
-        DataColumn dcGenre = new DataColumn("genre");
-        DataColumn dcTime = new DataColumn("time");
-        DataColumn dcFile = new DataColumn("file");
+        public DataSet GetAllSongs(string file)
+        {
+            DataSet ds = new DataSet("playlist");
 
-        dtSongs.Columns.Add(dcId);
-        dtSongs.Columns.Add(dcArtist);
-        dtSongs.Columns.Add(dcTitle);
-        dtSongs.Columns.Add(dcYear);
-        dtSongs.Columns.Add(dcGenre);
-        dtSongs.Columns.Add(dcTime);
-        dtSongs.Columns.Add(dcFile);
-        ds.Tables.Add(dtSongs);
+            DataTable dtSongs = new DataTable("songs");
 
-        ds.ReadXml(file);
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+            DataColumn dcYear = new DataColumn("year");
+            DataColumn dcGenre = new DataColumn("genre");
+            DataColumn dcTime = new DataColumn("time");
+            DataColumn dcFile = new DataColumn("file");
 
-        ds.ReadXml(HttpContext.Current.Server.MapPath(file));
-        return ds;
+            dtSongs.Columns.Add(dcId);
+            dtSongs.Columns.Add(dcArtist);
+            dtSongs.Columns.Add(dcTitle);
+            dtSongs.Columns.Add(dcYear);
+            dtSongs.Columns.Add(dcGenre);
+            dtSongs.Columns.Add(dcTime);
+            dtSongs.Columns.Add(dcFile);
+            ds.Tables.Add(dtSongs);
+
+            ds.ReadXml(file);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath(file));
+            return ds;
+        }
     }
 }
