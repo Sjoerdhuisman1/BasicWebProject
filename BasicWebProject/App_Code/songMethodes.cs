40c0b55aaabd046106c062a01902880e30d7acc9
Sjoerd Huisman <97072541@st.deltion.nl>
2020-02-28 12:51:34 +0100

    hallo

---
+++ BasicWebProject/App_Code/songMethodes.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace BasicWebProject.App_Code
+{
+    public class songMethodes
+    {
+    }
+
+    public DataSet GetAllSongs(string file)
+    {
+        DataSet ds = new DataSet("playlist");
+
+        DataTable dtSongs = new DataTable("songs");
+        
+        DataColumn dcId = new DataColumn("id");
+        DataColumn dcArtist = new DataColumn("artist");
+        DataColumn dcTitle = new DataColumn("title");
+        DataColumn dcYear = new DataColumn("year");
+        DataColumn dcGenre = new DataColumn("genre");
+        DataColumn dcTime = new DataColumn("time");
+        DataColumn dcFile = new DataColumn("file");
+    }
+}
